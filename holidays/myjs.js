13ded9d0206835f0851daed19ce0712983e006a1
Jspeaker5 <justin.speaker@greenwichschools.org>
2016-12-13 12:11:17 -0500

    Update myjs.js

--- holidays/myjs.js
+++ holidays/myjs.js
@@ -80,5 +80,22 @@ function generatePageData(pageData){
   $(video).attr('src', 'https://www.youtube.com/embed/pJBRQQkT85s');
     $(contentBox).append(video);
     }
+var modal = document.getElementById('myModal');
+
+
+var img = document.getElementById('thePhoto');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
 
 
